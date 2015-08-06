0907bd396b3cdfce87afb301174e6b86c734cc28
easyw <easyw@katamail.com>
2015-08-07 00:24:20 +0200

    added cadquery test parametric script

---
+++ cadquery/README.txt
@@ -0,0 +1,8 @@
+Note: cadquery-freecad-module required
+
+to run the demo:
+Linux:
+freecad cq-ex2.FCMacro
+
+windows:
+c:\FreeCAD\bin\freecad cq-ex2.FCMacro
---
+++ cadquery/launch-cq.bat
@@ -0,0 +1,8 @@
+::taskkill -im freecad.exe /f
+@echo OFF
+echo cadquery-freecad-module required
+@echo ON
+cd %~p0
+start "" "c:\FreeCAD\bin\freecad" cq-ex2.FCMacro
+
+pause
