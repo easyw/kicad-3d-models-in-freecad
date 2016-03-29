bca96e179f42c94239f68e23345562217f79d5bd
Oliver <oliver.henry.walters@gmail.com>
2016-03-29 22:11:16 +1100

    Added helper functions to cq_cad_tools.py Generated multiple pin-header types

--- cadquery/FCAD_script_generator/make_pinheaders_export_fc.py
+++ cadquery/FCAD_script_generator/make_pinheaders_export_fc.py
@@ -57,6 +57,8 @@ ___ver___ = "1.4.1 14/08/2015"
 from math import tan, radians, sqrt
 from collections import namedtuple
 
+from cq_cad_tools import say, sayw, saye
+
 import sys, os
 
 # maui start
@@ -116,174 +118,286 @@ import cadquery as cq
 from Helpers import show
 # maui end
 
-npins = 5          # number of pins
-sep = 2.54         # distance between pins
-chamfer = 0.4      # chamfering of plastic base
-hb = 2.54          # height of plastic base
-hb2 = 0.5          # height of bottom cut
-pw = 0.64          # width of a pin
-pw_tip = pw/2      # width of pin at the tip
-hp = 11.54         # height of a pin
-h_tip = pw_tip*1.5 # height of pins tip
-pos_y = -3         # vertical start position of a pin
-delta =sep/1000    # added delta for avoid geometry problems in fusion maui
+from collections import namedtuple
 
-case_color = (50, 50, 50)
-pins_color = (255, 215, 0)
+"""
+Parameters for creating various pin-headers
+"""
+Params = namedtuple("Params", [
+    'p', # pitch (separaration between pins)
+    'rows', #number of rows
+    'w', #width of plastic base
+    'c', # chamfering of plastic base
+    'h', # height of plastic base above board
+    'pw', #pin width (square pins only)
+    'pc', #pin end chamfer amount
+    'pa', #pin height above board
+    'ph', #pin height total
+    'rot', #rotation if required
+])
+
+headers = {
+    '254single': Params(
+        p = 2.54,
+        w = 2.5,
+        rows = 1,
+        c = 0.25,
+        h = 2.5,
+        pw = 0.64,
+        pc = 0.15,
+        pa = 11,
+        ph = 11 + 3.3,
+        rot = -90,
+    ),
+    '254dual': Params(
+        p = 2.54,
+        w = 5.0,
+        rows = 2,
+        c = 0.25,
+        h = 2.5,
+        pw = 0.64,
+        pc = 0.15,
+        pa = 11,
+        ph = 11 + 3.3,
+        rot = -90,
+    ),
+    #2.00mm pitch, single row
+    #e.g. http://multimedia.3m.com/mws/media/438474O/3mtm-pin-strip-header-ts2156.pdf
+    '200single': Params(
+        p = 2.00,
+        w = 2.0,
+        rows = 1,
+        c = 0.25,
+        h = 1.5,
+        pw = 0.5,
+        pc = 0.1,
+        pa = 5.9,
+        ph = 8.7,
+        rot = -90,
+    ),
+    #2.00mm pitch, dual row
+    #e.g. http://multimedia.3m.com/mws/media/438474O/3mtm-pin-strip-header-ts2156.pdf
+    '200dual': Params(
+        p = 2.00,
+        w = 4.0,
+        rows = 2,
+        c = 0.25,
+        h = 1.5,
+        pw = 0.5,
+        pc = 0.1,
+        pa = 5.9,
+        ph = 8.7,
+        rot = -90,
+    ),
+    #1.27mm pitch, single row
+    #e.g. http://www.sullinscorp.com/drawings/71_GRPB___1VWVN-RC,_10957-C.pdf
+    '127single': Params(
+        p = 1.27,
+        w = 2.14,
+        rows = 1,
+        c = 0.2,
+        h = 1.0,
+        pw = 0.4,
+        pc = 0.1,
+        pa = 4.0,
+        ph = 6.3,
+        rot = -90,
+    ),
+    #1.27mm pitch, dual row
+    #e.g. http://www.sullinscorp.com/drawings/71_GRPB___1VWVN-RC,_10957-C.pdf
+    '127dual': Params(
+        p = 1.27,
+        w = 3.4,
+        rows = 2,
+        c = 0.2,
+        h = 1.0,
+        pw = 0.4,
+        pc = 0.1,
+        pa = 4.0,
+        ph = 6.3,
+        rot = -90,
+    ),
+}
+
 destination_dir="./generated_pinheaders/"
-rotation = 0
+
 outdir = "" # handled below
 
-def make_pinheader(npins):
-    # create the base
-    # left edge
-    points = [
-        (-sep/2-delta/2, -sep/2-delta/2+chamfer),
-        (-sep/2-delta/2, sep/2+delta/2-chamfer)
-        ]
+#Make a single plastic base block (chamfered if required)
+def MakeBaseBlock(params):
+    block = cq.Workplane("XY").rect(params.p, params.w).extrude(params.h)
+    
+    #chamfer
+    if params.c > 0:
+        block = block.edges("Z+").chamfer(params.c)
+    
+    return block
+    
+#Make the plastic base
+#Note - making the 'blocks' separately and then making a UNION of the blocks seems to be the best way to get them to merge
+#make the plastic base
+def MakeBase(n, params):
 
-    # top edges
-    for i in range(npins):
-        points.append((-sep/2-delta/2+chamfer+(sep)*i, sep/2+delta/2))
-        points.append((sep/2+delta/2-chamfer+(sep)*i, sep/2+delta/2))
-        points.append((sep/2+delta/2+(sep)*i, sep/2+delta/2-chamfer))
+    base = MakeBaseBlock(params)
+    
+    for i in range(1,n):
+        block = MakeBaseBlock(params).translate((i*params.p,0,0))
+        base = base.union(block)
+        
+    #move the base to the 'middle'
+    if params.rows > 1:
+        offset = params.p * (params.rows - 1) / 2.0
+        base = base.translate((0,offset,0))
+        
+    return base
+    
+#make a single pin
+def MakePin(params):
+    pin = cq.Workplane("XY").workplane(offset=params.pa-params.ph).rect(params.pw,params.pw).extrude(params.ph)
 
-    # right edge
-    points.append((sep/2+delta/2+(sep)*(npins-1), -sep/2-delta/2+chamfer))
+    #chamfer each end of the pin if required
+    if params.pc > 0:
+        pin = pin.faces("<Z").chamfer(params.pc)
+        pin = pin.faces(">Z").chamfer(params.pc)
+    
+    return pin
+    
+#make all the pins
+def MakePinRow(n, params):
+    #make some pins
+    pin = MakePin(params)
+    
+    for i in range(1,n):
+        pin = pin.union(MakePin(params).translate((params.p * i,0,0)))
+    
+    return pin
 
-    # bottom edges
-    for i in reversed(range(npins)):
-        points.append((sep/2+delta/2-chamfer+(sep)*i, -sep/2-delta/2))
-        points.append((-sep/2-delta/2+chamfer+(sep)*i, -sep/2-delta/2))
-        points.append((-sep/2-delta/2+(sep)*i, -sep/2-delta/2+chamfer))
+#generate a name for the pin header
+def HeaderName(n, params):
+    return "PinHeader_Straight_{r:01}x{n:02}x{p:.2f}mm".format(r=params.rows,n=n,p=params.p)
+    
+#make a pin header using supplied parameters, n pins in each row
+def MakeHeader(n, params):
+    
+    name = HeaderName(n,params)
+    
+    #having a period '.' character in the model name REALLY messes with things.
+    docname = name.replace(".","")
+    
+    newdoc = FreeCAD.newDocument(docname)
+    App.setActiveDocument(docname)
+    Gui.ActiveDocument=Gui.getDocument(docname)
+    
+    pins = MakePinRow(n,params)
+    
+    #duplicate pin rows
+    if params.rows > 1:
+        for i in range(1,params.rows):
+            pins = pins.union(MakePinRow(n,params).translate((0,i*params.p,0)))
+    
+    base = MakeBase(n,params)
+        
+    #assign some colors
+    base_color = (50,50,50)
+    pins_color = (225,175,0)
 
-    base = cq.Workplane("front").polyline(points).extrude(hb)
-    # pin holes
-    base = base.faces(">Z").workplane().rarray(sep, 1, npins, 1).rect(pw, pw).cutThruAll()
-    # bottom cut
-    base = base.faces("<X").workplane().center(0,-(hb-hb2)/2).rect(sep-2*chamfer, hb2).cutThruAll()
-    # barely fillet the top
-    base = base.faces(">Z").fillet(0.1)
-    base=base.translate((-npins*sep/2+sep/2,0,0)) #maui
-
-    # make pins
-    def makePin():
-        pin = cq.Workplane(cq.Plane.named("front",(0,0,pos_y))). \
-            rect(pw_tip, pw_tip).workplane(offset=h_tip).rect(pw,pw).workplane(offset=hp-h_tip*2). \
-            rect(pw,pw).workplane(offset=h_tip).rect(pw_tip, pw_tip).loft(ruled=True)
-        return pin
-
-    pin = makePin()
-    pins = pin.translate((0,0,0))
-    for i in range(1,npins):
-        pins = pins.union(pin.translate((sep*i,0,0)))
-    pins = pins.translate((-npins*sep/2+sep/2,0,0))
-
-    return (base, pins)
-
-## def shapeToMesh(shape, color):
-##     mesh_data = shape.tessellate(1)
-##     return Mesh(points = mesh_data[0],
-##                 faces = mesh_data[1],
-##                 color = color)
-
-def make_one(npins):
-    """Creates a pin header model file for given number of pins."""
-    print("Generating pinheader %d" % npins)
-    case, pins = make_pinheader(npins)
-    ## exportX3D([shapeToMesh(case.toFreecad(), case_color),
-    ##            shapeToMesh(pins.toFreecad(), pins_color)],
-    ##           outdir+"/pinheader_1x%d.x3d" % npins)
-    print("Done pinheader %d" % npins)
-
-def make_all():
-    """Creates all straight pin headers from 1 to 40 pins in X3D format in
-    parallel."""
-    global outdir
-    outdir = os.path.abspath("./generated_pinheaders/")
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-
-    ## from multiprocessing import Pool
-    ##
-    ## p = Pool()
-    ## p.map(make_one, list(range(1,40+1)))  maui TBD
-
-
-def build_and_save(i):
-
-    ModelName='pinstrip_p254_'+str(i)+'x1'
-    Newdoc = FreeCAD.newDocument(ModelName)
-    App.setActiveDocument(ModelName)
-    Gui.ActiveDocument=Gui.getDocument(ModelName)
-
-    FreeCAD.Console.PrintMessage('making all pin headers\r\n')
-    case, pins = make_pinheader(i)
-
-    color_attr=case_color+(0,)
-    show(case, color_attr)
-    #FreeCAD.Console.PrintMessage(pins_color)
-    color_attr=pins_color+(0,)
-    #FreeCAD.Console.PrintMessage(color_attr)
-    show(pins, color_attr)
+    show(base,base_color+(0,))
+    show(pins,pins_color+(0,))
+    
     doc = FreeCAD.ActiveDocument
+        
     objs=GetListOfObjects(FreeCAD, doc)
     FuseObjs_wColors(FreeCAD, FreeCADGui,
-                    doc.Name, objs[0].Name, objs[1].Name)
-    doc.Label=ModelName
+                   doc.Name, objs[0].Name, objs[1].Name)
+    doc.Label=docname
     objs=GetListOfObjects(FreeCAD, doc)
-    objs[0].Label=ModelName
+    objs[0].Label=docname
     restore_Main_Tools()
-    #rotate if required
-    if (rotation!=0):
-        z_RotateObject(doc, rotation)
-    out_dir=destination_dir
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    #out_dir="./generated_qfp/"
-    # export STEP model
-    exportSTEP(doc,ModelName,out_dir)
-    # scale and export Vrml model
-    scale=0.3937001
-    exportVRML(doc,ModelName,scale,out_dir)
-    # Save the doc in Native FC format
-    saveFCdoc(App, Gui, doc, ModelName,out_dir)
-    #display BBox
 
-    FreeCADGui.ActiveDocument.getObject("Part__Feature").BoundingBox = True
-    #import ImportGui
-    #ImportGui.insert(u"C:/Cad/Progetti_K/FreeCAD-models/scripts/3D-models/CadQuery/reference-block.step","mypin")
+    if (params.rot !=0):
+        z_RotateObject(doc, params.rot)
+    
+    out_dir = "./generated_pinheaders/"
+    
+    doc.Label = docname
+    
+    #save the STEP file
+    exportSTEP(doc, name, out_dir)
+
+    #save the VRML file
+    scale=0.3937001
+    exportVRML(doc,name,scale,out_dir)
+    
+    # Save the doc in Native FC format
+    saveFCdoc(App, Gui, doc, name,out_dir)
 
     Gui.SendMsgToActiveView("ViewFit")
     Gui.activeDocument().activeView().viewAxometric()
 
     return 0
-# when run from command line
+    
 if __name__ == "__main__":
-    # make_all() maui TBD
-
-    FreeCAD.Console.PrintMessage('\r\nRunning...\r\n')
-# maui     run()
-
+    
+    models = []
+    
     if len(sys.argv) < 3:
-        FreeCAD.Console.PrintMessage('No variant name is given! building pinstrip_p254_5x1')
-        model_to_build='5'
+        say("Nothing to build...")
+        say("Specify name and pins")
+        
     else:
-        model_to_build=sys.argv[2]
-
-    if model_to_build == "all":
-        for i in range(1, 41):
-            build_and_save(i)
+        model = sys.argv[2]
+        
+        if model == 'all':
+            models = [headers[model] for model in headers.keys()]
+        else:
+            models = [headers[i] for i in model.split(',') if i in headers.keys()]#separate model types with comma
+            
+    pins = []
+    
+    if len(sys.argv) < 4:
+        say("No pins specifed. Not building")
     else:
-        build_and_save(int(model_to_build))
-
-
+        p = sys.argv[3].strip()
+        
+        #comma separarated pin numberings
+        if ',' in p:
+            try:
+                pins = map(int,p.split(','))
+            except:
+                say("Pin argument '",p,"' is invalid ,")
+                pins = []
+        
+        #range of pins x-y
+        elif '-' in p and len(p.split('-')) == 2:
+            ps = p.split('-')
+            
+            try:
+                p1, p2 = int(ps[0]),int(ps[1])
+                pins = range(p1,p2+1)
+            except:
+                say("Pin argument '",p,"' is invalid -")
+                pins = []
+                
+        #otherwise try for a single pin
+        else:
+            try:
+                pin = int(p)
+                pins = [pin]
+            except:
+                say("Pin argument '",p,"' is invalid")
+                pins = []
+                
+    #make all the seleted models
+    for model in models:
+        for pin in pins:
+            MakeHeader(pin,model)
 
 
 
 # when run from freecad-cadquery
 if __name__ == "temp.module":
-    ModelName="mypin"
+    pass
+    #ModelName="mypin"
     ## Newdoc = FreeCAD.newDocument(ModelName)
     ## App.setActiveDocument(ModelName)
     ## Gui.ActiveDocument=Gui.getDocument(ModelName)
