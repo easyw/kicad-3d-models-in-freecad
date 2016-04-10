fa75d1ae9067e890455260bddf246f931aa0c500
Rene Pöschl <r.poeschl@student.tugraz.at>
2016-04-10 12:48:01 +0200

    licence stuff.

--- cadquery/FCAD_script_generator/export_conn_jst_xh.py
+++ cadquery/FCAD_script_generator/export_conn_jst_xh.py
@@ -2,12 +2,8 @@
 #!/usr/bin/python
 #
 # This was originaly derived from a cadquery script for generating PDIP models in X3D format#
-#
 # from https://bitbucket.org/hyOzd/freecad-macros
 # author hyOzd
-# This is a
-# Dimensions are from Microchips Packaging Specification document:
-# DS00000049BY. Body drawing is the same as QFP generator#
 #
 # Adapted by easyw for step and vrlm export
 # See https://github.com/easyw/kicad-3d-models-in-freecad
@@ -16,22 +12,22 @@
 ## cadquery FreeCAD plugin
 ##   https://github.com/jmwright/cadquery-freecad-module
 
-## to run the script just do: freecad make_gwexport_fc.py modelName
+## to run the script just do: freecad scriptName modelName
 ## e.g. FreeCAD export_conn_jst_xh.py all
 
 ## the script will generate STEP and VRML parametric models
 ## to be used with kicad StepUp script
 
-#* These are a FreeCAD & cadquery tools                                     *
+#* These are FreeCAD & cadquery tools                                       *
 #* to export generated models in STEP & VRML format.                        *
 #*                                                                          *
-#* cadquery script for generating QFP/SOIC/SSOP/TSSOP models in STEP AP214  *
+#* cadquery script for generating JST-XH models in STEP AP214               *
 #*   Copyright (c) 2016                                                     *
 #* Rene Poeschl https://github.com/poeschlr                                 *
 #* All trademarks within this guide belong to their legitimate owners.      *
 #*                                                                          *
 #*   This program is free software; you can redistribute it and/or modify   *
-#*   it under the terms of the GNU General Public License (GPL)             *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)     *
 #*   as published by the Free Software Foundation; either version 2 of      *
 #*   the License, or (at your option) any later version.                    *
 #*   for detail see the LICENCE text file.                                  *
@@ -48,9 +44,10 @@
 #*                                                                          *
 #****************************************************************************
 
-__title__ = "make 3D models of JST-XH-Connectors types B??B-XH-A. (Top entry)"
+__title__ = "make 3D models of JST-XH-Connectors."
 __author__ = "scripts: maurice and hyOzd; models: poeschlr"
-__Comment__ = 'make 3D models of JST-XH-Connectors types B??B-XH-A. (Top entry)'
+__Comment__ = '''make 3D models of JST-XH-Connectors types B??B-XH-A. (Top entry),
+                S??B-XH-A (Side entry) and S??B-XH-A-1 (Side entry compact version).'''
 
 ___ver___ = "1.1 10/04/2016"
 
@@ -70,9 +67,9 @@ STR_licPreProc = ""
 LIST_license = ["Copyright (C) "+datetime.now().strftime("%Y")+", " + STR_licAuthor,
                 "",
                 "This program is free software: you can redistribute it and/or modify",
-                "it under the terms of the GNU General Public License as published by",
-                "the Free Software Foundation, either version 2 of the License, or",
-                "any later version.",
+                "it under the terms of the GNU Lesser General Public License (LGPL)",
+                "as published by the Free Software Foundation, either version 2 of",
+                "the License, or any later version.",
                 "",
                 "This program is distributed in the hope that it will be useful,",
                 "but WITHOUT ANY WARRANTY; without even the implied warranty of",
@@ -89,7 +86,7 @@ body_color = shaderColors.named_colors[body_color_key].getDiffuseInt()
 pins_color_key = "metal grey pins"
 pins_color = shaderColors.named_colors[pins_color_key].getDiffuseInt()
 
-destination_dir="Connectors_JST" #for now
+destination_dir="Connectors_JST"
 
 if FreeCAD.GuiUp:
     from PySide import QtCore, QtGui
@@ -128,7 +125,7 @@ import cq_cad_tools
 reload(cq_cad_tools)
 # Explicitly load all needed functions
 from cq_cad_tools import FuseObjs_wColors, GetListOfObjects, restore_Main_Tools, \
- exportSTEP, close_CQ_Example, exportVRML, saveFCdoc, z_RotateObject
+ exportSTEP, close_CQ_Example, saveFCdoc, z_RotateObject
 
 # Gui.SendMsgToActiveView("Run")
 Gui.activateWorkbench("CadQueryWorkbench")
@@ -145,8 +142,6 @@ import FreeCAD, Draft, FreeCADGui
 import ImportGui
 sys.path.append("cq_models")
 import conn_jst_xh_models as M
-
-# maui end
 
 if __name__ == "__main__":
 
@@ -209,8 +204,4 @@ if __name__ == "__main__":
         fusion = FuseObjs_wColors(FreeCAD, FreeCADGui,
                         ModelName, objs[0].Name, objs[1].Name, keepOriginals=True)
         exportSTEP(doc,FileName,out_dir,fusion)
-        #exportVRML(doc,FileName,scale,out_dir)
-        #fusion = FuseObjs_wColors(FreeCAD, FreeCADGui,
-        #                ModelName,objs)
-        #exportSTEP(doc,FileName,out_dir,[fusion])
         saveFCdoc(App, Gui, doc, FileName,out_dir)
