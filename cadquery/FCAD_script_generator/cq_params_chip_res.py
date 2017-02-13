eddce7d996f49c50f567e8ffdb781f22cde0be7a
Unknown <Frank@danishaviationsystems.dk>
2017-02-13 19:11:46 +0100

    added VRML material properties and parameters for Kicad naming

--- cadquery/FCAD_script_generator/cq_params_chip_res.py
+++ cadquery/FCAD_script_generator/cq_params_chip_res.py
@@ -12,11 +12,6 @@
 
 from collections import namedtuple
 
-# case_color = (139, 69, 19)  #  saddle brown
-# case_color = (139, 119, 101)  # peach puff 4
-top_color = (25, 25, 25)  # black
-case_color = (255, 255, 255)  # white
-pins_color = (230, 230, 230)
 destination_dir="/generated_res"
 
 ### Parametric Values
@@ -34,7 +29,7 @@ Params = namedtuple("Params", [
     'rotation' # rotation if required
 ])
 
-all_params = {
+all_params_res = {
     "0402_h040" : Params(
         L = 1.07,  # package length
         W = 0.55,  # package width
@@ -95,4 +90,247 @@ all_params = {
         modelName = 'r_2010_h084',  # Model Name
         rotation = 0   # rotation
     ),    
-}
+}   
+
+kicad_naming_params_res = {
+    "R_0201" : Params(
+        L = 0.63,  # package length
+        W = 0.33,  # package width
+        T = 0.23,  # package height
+
+        pb = 0.15,  # pin band
+        pt = 0.02,   # pin thickness
+
+        ef = 0.02, # fillet of edges
+        modelName = 'R_0201',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_0402" : Params(
+        L = 1.07,  # package length
+        W = 0.55,  # package width
+        T = 0.40,  # package height
+
+        pb = 0.32,  # pin band
+        pt = 0.025,   # pin thickness
+
+        ef = 0.02, # fillet of edges
+        modelName = 'R_0402',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_0603" : Params(
+        L = 1.55,  # package length
+        W = 0.85,  # package width
+        T = 0.45,  # package height
+
+        pb = 0.40,  # pin band
+        pt = 0.025,   # pin thickness
+
+        ef = 0.020, # fillet of edges
+        modelName = 'R_0603',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_0603_h" : Params(
+        L = 1.55,  # package length
+        W = 0.85,  # package width
+        T = 0.45,  # package height
+
+        pb = 0.40,  # pin band
+        pt = 0.025,   # pin thickness
+
+        ef = 0.020, # fillet of edges
+        modelName = 'R_0603_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_0805" : Params(
+        L = 2.00,  # package length
+        W = 1.25,  # package width
+        T = 0.50,  # package height
+
+        pb = 0.40,  # pin band
+        pt = 0.025,   # pin thickness
+
+        ef = 0.020, # fillet of edges
+        modelName = 'R_0805',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_0805_h" : Params(
+        L = 2.00,  # package length
+        W = 1.25,  # package width
+        T = 0.50,  # package height
+
+        pb = 0.40,  # pin band
+        pt = 0.025,   # pin thickness
+
+        ef = 0.020, # fillet of edges
+        modelName = 'R_0805_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1206" : Params(
+        L = 3.20,  # package length
+        W = 1.60,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1206',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1206_h" : Params(
+        L = 3.20,  # package length
+        W = 1.60,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1206_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1210" : Params(
+        L = 3.20,  # package length
+        W = 2.5,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1210',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1210_h" : Params(
+        L = 3.20,  # package length
+        W = 2.5,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1210_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1218" : Params(
+        L = 3.20,  # package length
+        W = 4.8,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1218',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1218_h" : Params(
+        L = 3.20,  # package length
+        W = 4.8,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1218_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1812" : Params(
+        L = 4.5,  # package length
+        W = 3.2,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1812',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_1812_h" : Params(
+        L = 4.5,  # package length
+        W = 3.2,  # package width
+        T = 0.55,  # package height
+
+        pb = 0.5,  # pin band
+        pt = 0.03,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_1812_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_2010" : Params(
+        L = 5.33,  # package length
+        W = 2.54,  # package width
+        T = 0.84,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2010',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_2010_h" : Params(
+        L = 5.33,  # package length
+        W = 2.54,  # package width
+        T = 0.84,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2010_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_2512" : Params(
+        L = 6.4,  # package length
+        W = 3.2,  # package width
+        T = 0.6 ,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2512',  # Model Name
+        rotation = 0   # rotation
+    ),  
+    "R_2512_h" : Params(
+        L = 6.4,  # package length
+        W = 3.2,  # package width
+        T = 0.6 ,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2512_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),  
+    "R_2816" : Params(
+        L = 6.4,  # package length
+        W = 4.2,  # package width
+        T = 0.65 ,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2816',  # Model Name
+        rotation = 0   # rotation
+    ),
+    "R_2816_h" : Params(
+        L = 6.4,  # package length
+        W = 4.2,  # package width
+        T = 0.65 ,  # package height
+
+        pb = 0.51,  # pin band
+        pt = 0.04,   # pin thickness
+
+        ef = 0.025, # fillet of edges
+        modelName = 'R_2816_HandSoldering',  # Model Name
+        rotation = 0   # rotation
+    ),
+}   
