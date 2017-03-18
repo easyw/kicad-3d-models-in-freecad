a9c30907eba57c422442a27704fab7fe6350e204
easyw <easyw@katamail.com>
2017-03-18 23:40:54 +0100

    fixed building in linux ... GullWings models

--- cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/main_generator.py
+++ cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/main_generator.py
@@ -440,11 +440,23 @@ def make_gw(params):
 #################################################
 import add_license as Lic
 
-if __name__ == "__main__":
+# when run from command line
+if __name__ == "__main__" or __name__ == "main_generator":
     expVRML.say(expVRML.__file__)
     FreeCAD.Console.PrintMessage('\r\nRunning...\r\n')
 
-# maui     run()
+    full_path=os.path.realpath(__file__)
+    expVRML.say(full_path)
+    scriptdir=os.path.dirname(os.path.realpath(__file__))
+    expVRML.say(scriptdir)
+    sub_path = full_path.split(scriptdir)
+    expVRML.say(sub_path)
+    sub_dir_name =full_path.split(os.sep)[-2]
+    expVRML.say(sub_dir_name)
+    sub_path = full_path.split(sub_dir_name)[0]
+    expVRML.say(sub_path)
+    models_dir=sub_path+"_3Dmodels"
+    
     color_pin_mark=True
     if len(sys.argv) < 3:
         FreeCAD.Console.PrintMessage('No variant name is given! building SOIC-8_3.9x4.9mm_Pitch1.27mm')
@@ -487,7 +499,7 @@ if __name__ == "__main__":
         ModelName = all_params[variant].modelName
         CheckedModelName = ModelName.replace('.', '')
         CheckedModelName = CheckedModelName.replace('-', '_')
-        Newdoc = FreeCAD.newDocument(CheckedModelName)
+        Newdoc = App.newDocument(CheckedModelName)
         App.setActiveDocument(CheckedModelName)
         Gui.ActiveDocument=Gui.getDocument(CheckedModelName)
         body, pins, mark = make_gw(all_params[variant])
@@ -532,7 +544,7 @@ if __name__ == "__main__":
             z_RotateObject(doc, rot)
         #out_dir=destination_dir+all_params[variant].dest_dir_prefix+'/'
         script_dir=os.path.dirname(os.path.realpath(__file__))
-        models_dir=script_dir+"/../_3Dmodels"
+        ## models_dir=script_dir+"/../_3Dmodels"
         expVRML.say(models_dir)
         out_dir=models_dir+destination_dir+os.sep+all_params[variant].dest_dir_prefix
         #out_dir=script_dir+os.sep+destination_dir
