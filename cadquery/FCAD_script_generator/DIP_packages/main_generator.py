13c7c513cc3816d101bdacdd4afb935db35badd7
easyw <easyw@katamail.com>
2017-03-20 21:08:41 +0100

    DIP fixed building in linux

--- cadquery/FCAD_script_generator/DIP_packages/main_generator.py
+++ cadquery/FCAD_script_generator/DIP_packages/main_generator.py
@@ -561,10 +561,23 @@ if __name__ == "temp.module":
 import add_license as Lic
 
 # when run from command line
-if __name__ == "__main__":
+if __name__ == "__main__" or __name__ == "main_generator":
 
     FreeCAD.Console.PrintMessage('\r\nRunning...\r\n')
-# maui     run()
+
+    full_path=os.path.realpath(__file__)
+    expVRML.say(full_path)
+    scriptdir=os.path.dirname(os.path.realpath(__file__))
+    expVRML.say(scriptdir)
+    sub_path = full_path.split(scriptdir)
+    expVRML.say(sub_path)
+    sub_dir_name =full_path.split(os.sep)[-2]
+    expVRML.say(sub_dir_name)
+    sub_path = full_path.split(sub_dir_name)[0]
+    expVRML.say(sub_path)
+    models_dir=sub_path+"_3Dmodels"
+    #expVRML.say(models_dir)
+    #stop
 
     if len(sys.argv) < 3:
         FreeCAD.Console.PrintMessage('No variant name is given! building DIP-8_W7.62mm')
@@ -590,7 +603,7 @@ if __name__ == "__main__":
         ModelName = all_params[variant].modelName
         CheckedModelName = ModelName.replace('.', '')
         CheckedModelName = CheckedModelName.replace('-', '_')
-        Newdoc = FreeCAD.newDocument(CheckedModelName)
+        Newdoc = App.newDocument(CheckedModelName)
         App.setActiveDocument(CheckedModelName)
         Gui.ActiveDocument=Gui.getDocument(CheckedModelName)
         #case, pins, pinmark = make_qfn(all_params[variant])
@@ -666,7 +679,7 @@ if __name__ == "__main__":
         # # export STEP model
         # exportSTEP(doc,ModelName,out_dir)
         script_dir=os.path.dirname(os.path.realpath(__file__))
-        models_dir=script_dir+"/../_3Dmodels"
+        #models_dir=script_dir+"/../_3Dmodels"
         expVRML.say(models_dir)
         out_dir=models_dir+destination_dir
         if not os.path.exists(out_dir):
