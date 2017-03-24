3fb7233694ddd46af54ba6e89e9347730096916e
Unknown <Frank@danishaviationsystems.dk>
2017-03-24 02:10:45 +0100

    added a few more

--- cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/cq_parameters_sot.py
+++ cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/cq_parameters_sot.py
@@ -131,6 +131,34 @@ all_params_sot = {
         ),
 }
 kicad_naming_params_sot = {
+	'Analog_KS-4': Params( # from http://www.analog.com/media/en/package-pcb-resources/package/pkg_pdf/sc70ks/ks_4.pdf
+        the = 8.0,      # body angle in degrees
+        tb_s = 0.05,    # top part of body is that much smaller
+        c = 0.15,        # pin thickness, body center part height
+        R1 = 0.1,       # pin upper corner, inner radius
+        R2 = 0.1,       # pin lower corner, inner radius
+        S = 0.00,       # pin top flat part length (excluding corner arc)
+#        L = 0.4,       # pin bottom flat part length (including corner arc)
+        fp_r = 0.15,     # first pin indicator radius
+        fp_d = 0.08,     # first pin indicator distance from edge
+        fp_z = 0.03,     # first pin indicator depth
+        ef = 0.0, #0.02,      # fillet of edges  Note: bigger bytes model with fillet
+        cc1 = 0.12, #0.45 chamfer of the 1st pin corner
+        D1 = 2.0,       # body length
+        E1 = 1.25,       # body width
+        E = 2.1,        # body overall width  E=E1+2*(S+L+c)
+        A1 = 0.1,  # body-board separation
+        A2 = 0.9,  # body height
+        b = 0.1,  # pin width
+        e = 0.1,  # pin (center-to-center) distance
+        npx = 16,   # number of pins along X axis (width)
+        npy = 0,   # number of pins along y axis (length)
+        epad = None, # e Pad
+        excluded_pins = (4,5,6,7,8,9,10,20,21,22,23,24,25,26,27,28,29), #no pin excluded
+        modelName = 'Analog_KS-4', #modelName
+        rotation = -90, # rotation if required
+        dest_dir_prefix = 'SOT'
+        ),
     'SC-59': Params( # from http://www.infineon.com/dgdl/SC59-Package_Overview.pdf?fileId=5546d462580663ef0158069ca21703c1
         the = 8.0,      # body angle in degrees
         tb_s = 0.05,    # top part of body is that much smaller
@@ -634,6 +662,62 @@ kicad_naming_params_sot = {
         epad = None, # e Pad
         excluded_pins = None, #no pin excluded
         modelName = 'SOT-23-8', #modelName
+        rotation = -90, # rotation if required
+        dest_dir_prefix = 'SOT'
+        ),
+    'SC-82AA': Params( # from http://media.digikey.com/pdf/Data%20Sheets/Rohm%20PDFs/da227.pdf
+        the = 8.0,      # body angle in degrees
+        tb_s = 0.05,    # top part of body is that much smaller
+        c = 0.15,        # pin thickness, body center part height
+        R1 = 0.1,       # pin upper corner, inner radius
+        R2 = 0.1,       # pin lower corner, inner radius
+        S = 0.00,       # pin top flat part length (excluding corner arc)
+#        L = 0.4,       # pin bottom flat part length (including corner arc)
+        fp_r = 0.15,     # first pin indicator radius
+        fp_d = 0.08,     # first pin indicator distance from edge
+        fp_z = 0.03,     # first pin indicator depth
+        ef = 0.0, #0.02,      # fillet of edges  Note: bigger bytes model with fillet
+        cc1 = 0.12, #0.45 chamfer of the 1st pin corner
+        D1 = 2.0,       # body length
+        E1 = 1.25,       # body width
+        E = 2.1,        # body overall width  E=E1+2*(S+L+c)
+        A1 = 0.1,  # body-board separation
+        A2 = 0.9,  # body height
+        b = 0.1,  # pin width
+        e = 0.1,  # pin (center-to-center) distance
+        npx = 17,   # number of pins along X axis (width)
+        npy = 0,   # number of pins along y axis (length)
+        epad = None, # e Pad
+        excluded_pins = (1,4,5,6,7,8,9,10,11,12,13,14,18,21,22,23,24,25,26,27,28,29,30,31,34), #no pin excluded
+        modelName = 'SC-82AA', #modelName
+        rotation = -90, # rotation if required
+        dest_dir_prefix = 'SOT'
+        ),
+    'SC-82AB': Params( # from http://www.infineon.com/dgdl/SOT343-Package_Overview.pdf?fileId=5546d462580663ef015806a5338d04ef
+        the = 8.0,      # body angle in degrees
+        tb_s = 0.05,    # top part of body is that much smaller
+        c = 0.15,        # pin thickness, body center part height
+        R1 = 0.1,       # pin upper corner, inner radius
+        R2 = 0.1,       # pin lower corner, inner radius
+        S = 0.00,       # pin top flat part length (excluding corner arc)
+#        L = 0.4,       # pin bottom flat part length (including corner arc)
+        fp_r = 0.15,     # first pin indicator radius
+        fp_d = 0.08,     # first pin indicator distance from edge
+        fp_z = 0.03,     # first pin indicator depth
+        ef = 0.0, #0.02,      # fillet of edges  Note: bigger bytes model with fillet
+        cc1 = 0.12, #0.45 chamfer of the 1st pin corner
+        D1 = 2.0,       # body length
+        E1 = 1.25,       # body width
+        E = 2.1,        # body overall width  E=E1+2*(S+L+c)
+        A1 = 0.1,  # body-board separation
+        A2 = 0.9,  # body height
+        b = 0.1,  # pin width
+        e = 0.1,  # pin (center-to-center) distance
+        npx = 16,   # number of pins along X axis (width)
+        npy = 0,   # number of pins along y axis (length)
+        epad = None, # e Pad
+        excluded_pins = (4,5,6,7,8,9,10,11,12,20,21,22,23,24,25,26,27,28,29), #no pin excluded
+        modelName = 'SC-82AB', #modelName
         rotation = -90, # rotation if required
         dest_dir_prefix = 'SOT'
         ),
