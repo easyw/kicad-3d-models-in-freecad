01c5f0cd0f20cffdb1668dfb3b9e90e7f7edd2fc
easyw <easyw@katamail.com>
2017-03-29 20:53:58 +0200

    added WIP folder with some experimenting files

---
+++ cadquery/FCAD_script_generator/WIP/README.md
@@ -0,0 +1,5 @@
+to be completed:
+* molded packages
+* flat-pins SOD
+
+added in molded the option to write only needed parameters (i.e. molded None default, True option)
---
+++ cadquery/FCAD_script_generator/WIP/create_model-molded.bat
@@ -0,0 +1,11 @@
+taskkill -im freecad.exe /f
+@echo OFF
+echo cadquery-freecad-module required
+@echo ON
+cd %~p0
+start "" "c:\FreeCAD\bin\freecad" main_generator_molded.py QFN-12-1EP_3x3mm_Pitch0.5mm
+:: QFN-20-1EP_3x4mm_Pitch0.5mm
+::QFN-12-1EP_3x3mm_Pitch0.5mm
+:: start "" "d:\FreeCAD_015\bin\freecad" main_generator.py %1
+
+::pause
