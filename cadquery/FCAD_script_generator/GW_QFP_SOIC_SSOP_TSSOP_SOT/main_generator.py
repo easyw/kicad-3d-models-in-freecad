1e1a7a1c186437c407b52561c7105a9a24e8268a
easyw <easyw@katamail.com>
2017-04-11 12:11:47 +0200

    SSOP: added ability to load footprint when generating 3d model

--- cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/main_generator.py
+++ cadquery/FCAD_script_generator/GW_QFP_SOIC_SSOP_TSSOP_SOT/main_generator.py
@@ -531,6 +531,7 @@ if __name__ == "__main__" or __name__ == "main_generator":
         save_memory=True
     elif model_to_build == "allSSOP":
         variants = all_params_ssop.keys()
+        footprints_dir="Housings_SSOP.pretty"
         save_memory=True
     elif model_to_build == "allTSSOP":
         variants = all_params_tssop.keys()
