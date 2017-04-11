4494accae754c3bb28e9c87220d0949c8c9ce738
Unknown <Frank@danishaviationsystems.dk>
2017-04-11 10:00:12 +0200

    more modelname checking and most SSOP parameters

--- cadquery/FCAD_script_generator/BGA_packages/main_generator.py
+++ cadquery/FCAD_script_generator/BGA_packages/main_generator.py
@@ -382,8 +382,7 @@ if __name__ == "__main__" or __name__ == "main_generator":
             print("Parameters for %s doesn't exist in 'all_params', skipping." % variant)
             continue
         ModelName = all_params[variant].modelName
-        CheckedModelName = ModelName.replace('.', '')
-        CheckedModelName = CheckedModelName.replace('-', '_')
+        CheckedModelName = ModelName.replace('.', '').replace('-', '_').replace('(', '').replace(')', '')
         Newdoc = App.newDocument(CheckedModelName)
         App.setActiveDocument(CheckedModelName)
         Gui.ActiveDocument=Gui.getDocument(CheckedModelName)
@@ -465,9 +464,9 @@ if __name__ == "__main__" or __name__ == "main_generator":
         ## print objs[0].Name, objs[1].Name, objs[2].Name
 
         ## sleep
-        doc.Label=ModelName
+        doc.Label=CheckedModelName
         objs=GetListOfObjects(FreeCAD, doc)
-        objs[0].Label=ModelName
+        objs[0].Label=CheckedModelName
         restore_Main_Tools()
         #rotate if required
         if (all_params[variant].rotation!=0):
