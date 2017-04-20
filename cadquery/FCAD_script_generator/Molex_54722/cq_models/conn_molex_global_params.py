c31ca69496276e0772a821ccd2b6691182b640f1
hackscribble <hackscribble@outlook.com>
2017-04-20 15:56:20 +0100

    Changed generated filename, based on feedback from librarians on footprint PR.

--- cadquery/FCAD_script_generator/Molex_54722/cq_models/conn_molex_global_params.py
+++ cadquery/FCAD_script_generator/Molex_54722/cq_models/conn_molex_global_params.py
@@ -9,8 +9,8 @@ packages_3d=lib_name+".3dshapes"+os.sep
 manufacturer_tag = "molex "
 
 def generate_footprint_name(series_name, part_name, num_pins, pin_pitch):
-    name = 'Molex_SlimStack_Receptacle_02x{:02d}_{:.1f}mm_{:s}-{:s}'.format(num_pins / 2, pin_pitch, series_name, part_name)
+    name = 'Molex_SlimStack_Receptacle_2x{:02d}_Pitch{:.1f}mm_{:s}-{:s}'.format(num_pins / 2, pin_pitch, series_name, part_name)
     return name
 
-# e.g. "Molex_SlimStack_Receptacle_02x08_0.5mm_54722-0164"
+# e.g. "Molex_SlimStack_Receptacle_2x08_Pitch0.5mm_54722-0164"
 
