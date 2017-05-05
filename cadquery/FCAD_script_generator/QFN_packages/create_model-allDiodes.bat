b63a62d334980da06e231ed065eadb15f725ac8a
easyw <easyw@katamail.com>
2017-05-06 01:12:10 +0200

    adding bat command to build all models

--- cadquery/FCAD_script_generator/QFN_packages/create_model-allDiodes.bat
+++ cadquery/FCAD_script_generator/QFN_packages/create_model-allDiodes.bat
@@ -4,7 +4,7 @@ echo cadquery-freecad-module required
 @echo ON
 cd %~p0
 start "" "c:\FreeCAD\bin\freecad" main_generator.py allDiodes
-start "" "c:\FreeCAD\bin\freecad" main_generator.py allQFN
+::start "" "c:\FreeCAD\bin\freecad" main_generator.py allQFN
 :: start "" "d:\FreeCAD_015\bin\freecad" main_generator.py %1
 
 ::pause
--- cadquery/FCAD_script_generator/QFN_packages/create_model-allQFN.bat
+++ cadquery/FCAD_script_generator/QFN_packages/create_model-allQFN.bat
@@ -3,7 +3,7 @@ taskkill -im freecad.exe /f
 echo cadquery-freecad-module required
 @echo ON
 cd %~p0
-start "" "c:\FreeCAD\bin\freecad" main_generator.py allDiodes
+::start "" "c:\FreeCAD\bin\freecad" main_generator.py allDiodes
 start "" "c:\FreeCAD\bin\freecad" main_generator.py allQFN
 :: start "" "d:\FreeCAD_015\bin\freecad" main_generator.py %1
 
