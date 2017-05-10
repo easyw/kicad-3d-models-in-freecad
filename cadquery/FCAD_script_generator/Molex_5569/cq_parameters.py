9801eb0fccd45774a352c8b928fd44b1ca2e3642
Tim Tashpulatov <tim.tashpulatov@gmail.com>
2017-05-10 21:41:17 +0300

    Add 8-pin part to Params

--- cadquery/FCAD_script_generator/Molex_5569/cq_parameters.py
+++ cadquery/FCAD_script_generator/Molex_5569/cq_parameters.py
@@ -22,16 +22,20 @@ Params = namedtuple("Params", [
 ])
 
 all_params_molex_5569 = {
-    "5569-2A2" : Params(
+    "5569-2A2": Params(
         N = 2,
         modelName = 'Molex_5569-2A2'  # Model Name
         ),
+    "5569-4A2": Params(
+        N = 4,
+        modelName = 'Molex_5569-4A2'  # Model Name
+        ),
+    "5569-6A2": Params(
+       N = 6,
+       modelName = 'Molex_5569-6A2'  # Model Name
+   ),
+    "5569-8A2": Params(
+       N = 8,
+       modelName = 'Molex_5569-8A2'  # Model Name
+   ),
 }
-#    "5569-4A2" : Params(
-#        N = 4,
-#        modelName = 'Molex_5569-4A2'  # Model Name
-#        ),
-#   "5569-6A2" : Params(
-#       N = 6,
-#       modelName = 'Molex_5569-6A2'  # Model Name
-#   ),
