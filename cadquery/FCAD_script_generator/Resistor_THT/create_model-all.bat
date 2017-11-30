6c28e89aa56784ee8f8c6f222fcd4a50c54e4068
grob6000 <grobinson6000@gmail.com>
2017-12-01 10:05:15 +1100

    Fix path in bat files
    My freecad is in a normal regular ordinary swedish directory.

--- cadquery/FCAD_script_generator/Resistor_THT/create_model-all.bat
+++ cadquery/FCAD_script_generator/Resistor_THT/create_model-all.bat
@@ -3,7 +3,7 @@ taskkill -im freecad.exe /f
 echo cadquery-freecad-module required
 @echo ON
 cd %~p0
-start "" "c:\Program Files\FreeCAD 0.16\bin\FreeCAD.exe" main_generator.py all
+start "" "d:\FreeCAD_015\bin\freecad" main_generator.py all
 
 
 ::pause
--- cadquery/FCAD_script_generator/Resistor_THT/create_model-example.bat
+++ cadquery/FCAD_script_generator/Resistor_THT/create_model-example.bat
@@ -4,6 +4,6 @@ echo cadquery-freecad-module required
 @echo ON
 cd %~p0
 
-start "" "c:\program files\freecad 0.16\bin\freecad.exe" main_generator.py R_Array_SIP
+start "" "d:\FreeCAD_015\bin\freecad" main_generator.py R_Array_SIP
 
 :: pause
