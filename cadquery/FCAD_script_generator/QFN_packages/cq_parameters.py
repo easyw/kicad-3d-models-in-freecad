bde0725c9bd4d3ad9cc9226f2b0157d71612545b
antonlysak <antonlysak@gmail.com>
2018-07-04 23:59:39 +0600

    Adding DFN-6-1EP_1.2x1.2mm_P0.4mm_EP0.3x0.94mm

--- cadquery/FCAD_script_generator/QFN_packages/cq_parameters.py
+++ cadquery/FCAD_script_generator/QFN_packages/cq_parameters.py
@@ -525,6 +525,32 @@ kicad_naming_params_qfn = {
         rotation = -90, # rotation if required
         dest_dir_prefix = '../Housings_DFN_QFN.3dshapes/'
         ),
+    'DFN-6-1EP_1.2x1.2mm_P0.4mm_EP0.3x0.94mm': Params( # from http://www.onsemi.com/pub/Collateral/NCP133-D.PDF
+        c = 0.025,        # pin thickness, body center part height
+#        K=0.2,          # Fillet radius for pin edges
+        L = 0.2,        # pin top flat part length (including fillet radius)
+        fp_s = True,     # True for circular pinmark, False for square pinmark (useful for diodes)
+        fp_r = 0.2,     # first pin indicator radius
+        fp_d = 0.08,     # first pin indicator distance from edge
+        fp_z = 0.01,     # first pin indicator depth
+        ef = 0.0, # 0.05,      # fillet of edges  Note: bigger bytes model with fillet
+        cce = 0.15,      #0.45 chamfer of the epad 1st pin corner
+        D = 1.2,       # body overall length
+        E = 1.2,       # body overall width
+        A1 = 0.02,  # body-board separation  maui to check
+        A2 = 0.375,  # body height
+        b = 0.18,  # pin width
+        e = 0.4,  # pin (center-to-center) distance
+        m = 0.0,  # margin between pins and body  
+        ps = 'square',   # rounded pads
+        npx = 3,  # number of pins along X axis (width)
+        npy = 0,  # number of pins along y axis (length)
+        epad = (0.94,0.3), # e Pad #epad = None, # e Pad
+        excluded_pins = None, #no pin excluded
+        modelName = 'DFN-6-1EP_1.2x1.2mm_P0.4mm_EP0.3x0.94mm', #modelName
+        rotation = -90, # rotation if required
+        dest_dir_prefix = '../Housings_DFN_QFN.3dshapes/'
+        ),
     'DFN-6-1EP_2x2mm_Pitch0.5mm': Params( # from http://cds.linear.com/docs/en/packaging/05081703_C_DC6.pdf
         c = 0.2,        # pin thickness, body center part height
 #        K=0.2,          # Fillet radius for pin edges
