1a66e766acc7ff5838753fa8fc70ce824f35eee3
Boris Gulay <boris@gulay.name>
2019-06-24 22:12:46 +0300

    Improve Python 3 compatibility

--- cadquery/FCAD_script_generator/Converter_ACDC/main_generator.py
+++ cadquery/FCAD_script_generator/Converter_ACDC/main_generator.py
@@ -92,7 +92,14 @@ STR_licOrg = "FreeCAD"
 # Import cad_tools
 import cq_cad_tools
 # Reload tools
-reload(cq_cad_tools)
+def reload_lib(lib):
+    if (sys.version_info > (3, 0)):
+        import importlib
+        importlib.reload(lib)
+    else:
+        reload (lib)
+
+reload_lib(cq_cad_tools)
 # Explicitly load all needed functions
 from cq_cad_tools import FuseObjs_wColors, GetListOfObjects, restore_Main_Tools, \
  exportSTEP, close_CQ_Example, exportVRML, saveFCdoc, z_RotateObject, Color_Objects, \
@@ -133,7 +140,7 @@ except NameError:
 try:
     close_CQ_Example(FreeCAD, Gui)
 except: # catch *all* exceptions
-    print "CQ 030 doesn't open example file"
+    print("CQ 030 doesn't open example file")
 
 
 destination_dir="/Converter_ACDC"
