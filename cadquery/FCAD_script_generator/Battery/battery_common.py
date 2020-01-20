69bd9cd2913c626a4c70833853cd2537ea2ece36
Carlos Nieves <cnieves.mail@gmail.com>
2020-01-20 11:05:43 +0100

    Fixed for Python3

--- cadquery/FCAD_script_generator/Battery/battery_common.py
+++ cadquery/FCAD_script_generator/Battery/battery_common.py
@@ -28,7 +28,11 @@ from collections import namedtuple
 # Import cad_tools
 import cq_cad_tools
 # Reload tools
-reload(cq_cad_tools)
+try:
+    reload(cq_cad_tools)
+except:
+    import importlib
+    importlib.reload(cq_cad_tools)
 # Explicitly load all needed functions
 from cq_cad_tools import FuseObjs_wColors, GetListOfObjects, restore_Main_Tools, \
  exportSTEP, close_CQ_Example, exportVRML, saveFCdoc, z_RotateObject, Color_Objects, \
@@ -61,7 +65,7 @@ except: # catch *all* exceptions
 try:
     close_CQ_Example(FreeCAD, Gui)
 except: # catch *all* exceptions
-    print "CQ 030 doesn't open example file"
+    print("CQ 030 doesn't open example file")
 
 
 
