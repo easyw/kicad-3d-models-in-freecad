c88fe779db061b9f00651ee8eb01ecf921821aa9
easyw <easyw2g@gmail.com>
2020-05-09 19:13:12 +0200

    adapting C_Chip_SMD generator to py3, >=FC0.18.4

--- cadquery/FCAD_script_generator/C_Chip_SMD/create_model-example.bat
+++ cadquery/FCAD_script_generator/C_Chip_SMD/create_model-example.bat
@@ -3,7 +3,7 @@ taskkill -im freecad.exe /f
 echo cadquery-freecad-module required
 @echo ON
 cd %~p0
-start "" "c:\FreeCAD_016_6712\bin\freecad" main_generator.py C_01005_0402Metric
-:: start "" "d:\FreeCAD_015\bin\freecad" main_generator.py %1
+start "" "c:\FreeCAD\bin\freecad" main_generator.py C_01005_0402Metric
+:: start "" "d:\FreeCAD\bin\freecad" main_generator.py %1
 
 ::pause
